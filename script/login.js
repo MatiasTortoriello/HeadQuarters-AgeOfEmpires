5bb433b3a9492f8ac0d2359a0c428050462d3cde
Matias Tortoriello <85110239+MatiasTortoriello@users.noreply.github.com>
2021-10-23 12:57:43 -0300

    login

--- script/login.js
+++ script/login.js
@@ -1,9 +1,9 @@
 function validate() {
     let username=document.getElementById("username").value;
     let password=document.getElementById("password").value;
-    if (username="user" && (password = "pass"))
+    if (username="USER" && (password = "PASS"))
     {
-        window.location.href = "pages/aoe2_main.html";
+        window.location.href = "/pages/aoe2_main.html";
     } else {
         alert ("Nope!");
     }
