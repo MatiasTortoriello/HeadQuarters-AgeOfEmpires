c053c73b1f11dce9811e7a8b8efd5d3ef180825c
Matias Tortoriello <85110239+MatiasTortoriello@users.noreply.github.com>
2021-11-03 10:41:38 -0300

    .

--- script/unitShowcase.js
+++ script/unitShowcase.js
@@ -5,3 +5,10 @@ showcaseTownCenter.addEventListener('click', (e) => {
     console.log(e.target
 })
 */
+
+let barracks = document.querySelector('#barracks')
+let archeryRange = document.querySelector('#archeryRange')
+let stable = document.querySelector('#stable')
+let dock = document.querySelector('#dock')
+let siegeWorkshop = document.querySelector('#siegeWorkshop')
+
